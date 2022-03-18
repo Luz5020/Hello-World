4c6d74590e2a70d9381b1aef0cd16b1bde80aee6
Luca F <43752735+Luz5020@users.noreply.github.com>
2022-03-18 17:28:33 +0100

    File Cleanup Indev02 first file

---
+++ Python/iseven.py
@@ -0,0 +1,9 @@
+# floatnumber = float(2)
+# x = floatnumber % 2
+number = input("Enter number now:\n")
+number = int(number)
+x = number % 2
+if x == 0:
+    print("number is even")
+else:
+    print("number is not even")
