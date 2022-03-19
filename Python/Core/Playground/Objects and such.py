c14fb2d53616b1b7575c807f8a9bb04aa8bbcffc
Luca <43752735+Luz5020@users.noreply.github.com>
2022-03-19 10:56:21 +0100

    Indev 03 Added a cat Minor typo fixes and Changelog update

--- Python/Core/Playground/Objects and such.py
+++ Python/Core/Playground/Objects and such.py
@@ -15,5 +15,10 @@ Cat1 = Cat()
 Cat1.name = "Zelda"
 Cat1.color = "black"
 Cat1.value = "infinity"
+Cat2 = Cat()
+Cat2.name = "Sternchen"
+Cat2.color = "brown"
+Cat2.value = "infinity"
 # Introduction of the Cats
 print(Cat1.description())
+print(Cat2.description())
