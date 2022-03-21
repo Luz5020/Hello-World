19c708caf02c74c1c4849e8ba58c3f5ed9b659ac
Luca <43752735+Luz5020@users.noreply.github.com>
2022-03-21 18:45:22 +0100

    Indev 04 Minor Change

--- Python/Core/Math/Math Usable.py
+++ Python/Core/Math/Math Usable.py
@@ -1,11 +1,12 @@
 import time
 
+import Coreload
 import Math
 
 # LoadingscreenFX
-# Coreload.loadingeffectstart()
-# Math.loadingscreen()
-# Coreload.loadingeffectend()
+Coreload.loadingeffectstart()
+Math.loadingscreen()
+Coreload.loadingeffectend()
 # End LoadingscreenFX
 print("Select Operation:")
 print("+++Basic Operations+++")
@@ -22,14 +23,19 @@ except ValueError:
     time.sleep(0.1)
 if X == 1:
     Math.addition()
+    input("Press Enter Key to Exit")
 elif X == 2:
     Math.subtraction()
+    input("Press Enter Key to Exit")
 elif X == 3:
     Math.multiplication()
+    input("Press Enter Key to Exit")
 elif X == 4:
     Math.division()
+    input("Press Enter Key to Exit")
 elif X == 5:
     Math.iseven()
+    input("Press Enter Key to Exit")
 else:
     print("Invalid Input")
     input("Press Enter Key to Exit")
