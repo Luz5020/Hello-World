b5e23d03d8567655c837300b05c32835a64f1d1a
Luca <43752735+Luz5020@users.noreply.github.com>
2022-03-21 19:13:46 +0100

    Indev 04 Final Push of the Day

--- Python/Writer.py
+++ Python/Writer.py
@@ -3,5 +3,5 @@ import datetime
 dts = datetime.datetime.now()
 dts = str(dts)
 P = open("Automated Patchlog.txt", "at")
-P.write("Automated logging requested at " + dts)
+P.write("Automated logging requested at " + dts + "\n")
 print("Added the following: Automated logging requested at " + dts)
