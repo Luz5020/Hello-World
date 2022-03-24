7f590cbfafce451961935d665dff69b018b845bc
Luca <43752735+Luz5020@users.noreply.github.com>
2022-03-24 16:42:31 +0100

    Indev05 I'm exhausted... Just created the NameGenerator C.O.R.E. Module from scratch Also pushing a .gitignore for private files. TODO: Add Loading FX to NameGen

--- Python/Playground/Playwright Testing.py
+++ Python/Playground/Playwright Testing.py
@@ -1,8 +1,8 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    browser = p.firefox.launch(headless=False, slow_mo=50)
+    browser = p.webkit.launch(headless=False, slow_mo=50)
     page = browser.new_page()
-    page.goto("https://whatsmyuseragent.org/")
+    page.goto("http://whatsmyuseragent.org/")
     page.screenshot(path="example.png")
     browser.close()
