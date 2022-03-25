40b3e171b5e58397cc155d9cc05296e36ef24cb8
Luca <43752735+Luz5020@users.noreply.github.com>
2022-03-25 16:32:03 +0100

    Delete Cutter.py

--- Python/Core/Naming/Cutter.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-col_list = ["yr", "sex", "FirstForename", "number", "rank", "position"]
-df = pd.read_csv("Names.csv", usecols=col_list)
-for x in df.index:
-    if df.loc[x, "yr"] < 2010:
-        df.drop(x, inplace=True)
-df.to_csv(r'C:\Users\lucaf\PycharmProjects\Hello-World\Python\Core\Naming\Names_Short.csv', index=False, header=True)
